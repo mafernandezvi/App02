c321cce45d82b984496c2392e936dcdea056d7ce
Israel Antezana Rojas <israelantezana@gmail.com>
2021-10-21 15:39:58 -0400

    Enlazando presenter con logica de negocio

--- src/codebreaker/presenter.js
+++ src/codebreaker/presenter.js
@@ -1,8 +1,10 @@
+import CodeBreaker from "./codebreaker.js";
 const numero_elem = document.querySelector("#numero");
 const boton_elem = document.querySelector("#arriesgo");
 const resultado = document.querySelector("#resultado");
 
 boton_elem.addEventListener("click", (event) => {
   event.preventDefault();
-  resultado.innerHTML = "Ganaste!";
+  const cb = new CodeBreaker();
+  resultado.innerHTML = cb.arriesgar(numero_elem.value);
 });
